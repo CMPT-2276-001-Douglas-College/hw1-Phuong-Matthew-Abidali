72b824cbc5c28fad8f3e0c90e7a9360ea44c188e
LostCauseSauce <lostcausesauce@gmail.com>
2023-02-09 12:48:15 -0800

    Added data validation and rep input request
    Also deleted redundant files

--- Visual Studio Project files/Assignment01Part02/Source.cpp
+++ Visual Studio Project files/Assignment01Part02/Source.cpp
@@ -7,40 +7,107 @@
 
 using namespace std;
 
-pair<vector<string>, vector<int>> readFile(string fileName)
+vector<pair<string, int>> readFile(string filePath, vector<int>& errorLines)
 {
-	if (fileName.substr(fileName.size() - 4) != ".csv")
-		throw runtime_error("Invalid file type.\n");
-	ifstream myFile(fileName);
+	if (filePath.size() < 5 || filePath.substr(filePath.size() - 4) != ".csv")
+		throw invalid_argument("Invalid file type.\n");
+	ifstream myFile(filePath);
+	if (myFile.fail())
+		throw invalid_argument("File doesn't exist at this location.\n");
 	string name;
-	int number;
-	getline(myFile, name, '\n');
-	pair<vector<string>, vector<int>> toReturn;
-	while (getline(myFile, name, ','))
+	string number;
+	string line;
+	int lineNumber = 1;
+	getline(myFile, line);
+	vector<pair<string, int>> toReturn;
+
+	while (getline(myFile, line))
 	{
-		myFile >> number;
-		toReturn.first.push_back(name);
-		toReturn.second.push_back(number);
-		myFile.ignore(1, '\n');
+		int i = 0;
+		int errorCount = errorLines.size();
+		for (; i < line.size(); i++)
+		{
+			if (isalpha(line[i]) || line[i] == ' ')
+				name += line[i];
+			else if (line[i] == ',')
+			{
+				i++;
+				if (name == "")
+					errorLines.push_back(++lineNumber);
+				break;
+			}
+			else
+			{
+				errorLines.push_back(++lineNumber);
+				name = "";
+				break;
+			}
+		}
+		if (errorLines.size() == errorCount)
+		{
+			for (; i <= line.size(); i++)
+			{
+				if (isdigit(line[i]))
+					number += line[i];
+				else if (line[i] == ',' || line[i] == '\0')
+				{
+					if (number == "")
+					{
+						errorLines.push_back(++lineNumber);
+						name = "";
+					}
+					break;
+				}
+				else
+				{
+					errorLines.push_back(++lineNumber);
+					name = "";
+					number = "";
+					break;
+				}
+			}
+			if (errorLines.size() == errorCount)
+			{
+				toReturn.push_back(make_pair(name, stoi(number)));
+				name = "";
+				number = "";
+			}
+		}
 	}
+	myFile.close();
+	if (toReturn.size() == 0)
+		throw invalid_argument("File has no data.\n");
 	return toReturn;
 }
 
 int main()
 {
 	cout << "File to read: ";
-	string inputFileName;
-	getline(cin, inputFileName);
-	pair<vector<string>, vector<int>> inputData;
+	string inputFilePath;
+	getline(cin, inputFilePath);
+	vector<pair<string, int>> inputData;
+	vector<int> errorLines;
 	try
 	{
-		inputData = readFile(inputFileName);
+		inputData = readFile(inputFilePath, errorLines);
 	}
 	catch (exception& e)
 	{
 		cout << e.what();
 		return 0;
 	}
-	//for (int i = 0; i < inputData.first.size(); i++)
-	//	cout << inputData.first[i] << ' ' << inputData.second[i] << '\n';
+	int repCount = 0;
+	while (repCount == 0)
+	{
+		cout << "Enter number of representatives: ";
+		cin >> repCount;
+		if (repCount < 1 || repCount > 1000)
+		{
+			cin.clear();
+			cin.ignore(10000, '\n');
+			cout << "Invalid number of representatives.\n";
+		}
+	}
+
+
 }
